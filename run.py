c95da5fa5ad8d37dac68afcc2be6bd9c0e34f13a
Allen Ho <allenho@Allens-MacBook-Air.local>
2024-10-11 23:35:39 +1300

    first commit

---
+++ run.py
@@ -0,0 +1,27 @@
+"""
+This example run script shows how to run the Instagram.com scraper defined in ./instagram.py
+It scrapes product data and product search and saves it to ./results/
+
+To run this script set the env variable $SCRAPFLY_KEY with your scrapfly API key:
+$ export $SCRAPFLY_KEY="your key from https://scrapfly.io/dashboard"
+"""
+from pathlib import Path
+import asyncio
+import json
+import instagram
+
+output = Path(__file__).parent / "results"
+output.mkdir(exist_ok=True)
+
+
+async def run():
+    # enable scrapfly cache?
+    instagram.BASE_CONFIG["cache"] = True
+    instagram.BASE_CONFIG["debug"] = True
+    print("running Instagram scrape and saving results to ./results directory")
+    post_multi_image = await instagram.scrape_post_with_httpx("https://www.instagram.com/p/C_9l_-cTFJV/?igsh=N2Mwd3JyOWYzamFl")
+    output.joinpath("food-multi-image-post-use-httpx-with-parse-post-function.json").write_text(json.dumps(post_multi_image, indent=2, ensure_ascii=False), encoding='utf-8')
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
